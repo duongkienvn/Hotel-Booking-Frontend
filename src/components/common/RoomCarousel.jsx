cac1ec62f1d7600aeafea3c254ddba442e8caf25
Duong Kien <duongcongkien2005@gmail.com>
2025-09-06 16:29:03 +0700

    modify database

--- src/components/common/RoomCarousel.jsx
+++ src/components/common/RoomCarousel.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {getAvailableRooms} from "../utils/ApiFunctions";
 import {Link} from "react-router-dom";
-import {Button, Card, Carousel, Col, Image, Row, Typography} from "antd";
+import {Card, Carousel, Col, Image, Row, Spin, Typography} from "antd";
 import {motion} from "framer-motion";
 import BookNowButton from "../room/BookNowButton.jsx";
 
@@ -26,66 +26,73 @@ const RoomCarousel = () => {
   }, []);
 
 
-  if (isLoading) {
-    return <div className="mt-5">Loading rooms....</div>;
-  }
   if (errorMessage) {
     return <div className="text-danger mb-5 mt-5">Error : {errorMessage}</div>;
   }
+  //
+  // if (isLoading) {
+  //   return <Spin tip={"Loading rooms"} fullscreen size={"large"} />;
+  // }
 
   return (
-    <section
-      className={"container room-carousel__container"}>
-      <Carousel autoplay autoplaySpeed={2000}>
-        {[...Array(Math.ceil(rooms.length / 4))].map((_, index) => (
-          <div key={index}>
-            <Row gutter={[16, 16]} justify="center">
-              {rooms.slice(index * 4, index * 4 + 4).map((room) => (
-                <Col xs={24} sm={12} md={12} lg={6} key={room.id}>
-                  <motion.div
-                    initial={{opacity: 0, y: 50}}
-                    whileInView={{opacity: 1, y: 0}}
-                    transition={{duration: 0.6, ease: "easeOut"}}
-                    viewport={{once: true}}
-                  >
-                    <Card
-                      hoverable
-                      cover={
-                        <Link to={`/book-room/${room.id}`}>
-                          <Image
-                            preview={false}
-                            alt="Room"
-                            src={`data:image/png;base64,${room.photo}`}
-                            style={{
-                              width: "100%",
-                              height: "200px",
-                              objectFit: "cover",
-                              borderRadius: "8px 8px 0 0",
-                            }}
+    isLoading ? (
+      <Spin tip={"Loading rooms"} size={"large"}/>
+    ) : (
+      <>
+        <section
+          className={"container room-carousel__container"}>
+          <Carousel autoplay autoplaySpeed={2000}>
+            {[...Array(Math.ceil(rooms.length / 4))].map((_, index) => (
+              <div key={index}>
+                <Row gutter={[16, 16]} justify="center">
+                  {rooms.slice(index * 4, index * 4 + 4).map((room) => (
+                    <Col xs={24} sm={12} md={12} lg={6} key={room.id}>
+                      <motion.div
+                        initial={{opacity: 0, y: 50}}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{duration: 0.6, ease: "easeOut"}}
+                        viewport={{once: true}}
+                      >
+                        <Card
+                          hoverable
+                          cover={
+                            <Link to={`/book-room/${room.id}`}>
+                              <Image
+                                preview={false}
+                                alt="Room"
+                                src={`data:image/png;base64,${room.photo}`}
+                                style={{
+                                  width: "100%",
+                                  height: "200px",
+                                  objectFit: "cover",
+                                  borderRadius: "8px 8px 0 0",
+                                }}
+                              />
+                            </Link>
+                          }
+                        >
+                          <Card.Meta
+                            title={<span className="hotel-color">{room.roomType}</span>}
+                            description={
+                              <>
+                                <Title level={5} style={{margin: "8px 0", color: "#52c41a"}}>
+                                  ${room.roomPrice} / night
+                                </Title>
+                                <BookNowButton roomId={room.id}/>
+                              </>
+                            }
                           />
-                        </Link>
-                      }
-                    >
-                      <Card.Meta
-                        title={<span className="hotel-color">{room.roomType}</span>}
-                        description={
-                          <>
-                            <Title level={5} style={{margin: "8px 0", color: "#52c41a"}}>
-                              ${room.roomPrice} / night
-                            </Title>
-                            <BookNowButton roomId={room.id}/>
-                          </>
-                        }
-                      />
-                    </Card>
-                  </motion.div>
-                </Col>
-              ))}
-            </Row>
-          </div>
-        ))}
-      </Carousel>
-    </section>
+                        </Card>
+                      </motion.div>
+                    </Col>
+                  ))}
+                </Row>
+              </div>
+            ))}
+          </Carousel>
+        </section>
+      </>
+    )
   );
 };
 
